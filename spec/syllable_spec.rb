70cbe70b7d7169c9575182b2059d75b37d1718a4
Masafumi Yokoyama <myokoym@gmail.com>
2012-10-07 03:56:45 +0900

    rspec: add context block

--- spec/syllable_spec.rb
+++ spec/syllable_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 
 describe MeCab::Syllable do
   subject { MeCab::Syllable }
+  context "#count" do
   it "めかぶ == 3" do
     subject.new("めかぶ").count.should == 3
   end
 
   it "今日は雨 == 5" do
     subject.new("今日は雨").count.should == 5
+  end
   end
 end
